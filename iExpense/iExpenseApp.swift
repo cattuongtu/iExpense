55c7e1386febe8ea221078fb9069e695e02630ef
cattuongtu <cattuongtu3@gmail.com>
2023-12-19 15:53:19 -0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Cat-Tuong Tu on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
